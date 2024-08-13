21fd067a40a4e178194506c69f349e8c7f15f821
sobaman <sobaman522@gmail.com>
2024-08-13 13:01:55 +0900

    [#2]할 일 조회 API 기능 구현

--- src/main/java/org/example/persist/TaskRepository.java
+++ src/main/java/org/example/persist/TaskRepository.java
@@ -1,8 +1,17 @@
 package org.example.persist;
 
 
+import org.example.constants.TaskStatus;
 import org.example.persist.entity.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
+@Repository
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+
+    List<TaskEntity> findAllByDueDate(Date dueDate);
+    List<TaskEntity> findAllByStatus(TaskStatus status);
 }
